e46bd20e3e813f2563fb9d668a2bf2373d89749c
Mathias <hmvege@ulrik.uio.no>
2017-04-23 11:15:03 -0400

    Created a class for finding analytical expressions to make things a little more smoother

--- analytical_expressions.py
+++ analytical_expressions.py
@@ -1,5 +1,50 @@
 import sympy as sy, numpy as np
 
+class DerFinder:
+	def __init__(self, expression):
+		self.expression = expression
+		self.substitutions = []
+
+	def add_subs(self, subs_expr):
+		"""
+		Arguments
+			subs_expr: 	Takes a list where each cell is a list of length 2
+		"""
+		self.substitutions = subs_expr
+
+	def _derivate(self, vars_to_derivate):
+		"""
+		Internal function used for derivating. Takes list of derivatives and adds them together.
+		"""
+		self.expression_differentiated = sum(sy.diff(self.expression, x) for x in vars_to_derivate)
+
+	def _substitute(self, expr):
+		"""
+		Internal function used when substituting expression
+		"""
+		if len(self.substitutions) == 0: return expr
+		for expr_element in self.substitutions:
+			expr = expr.subs(expr_element[0], expr_element[1])
+		return expr
+
+	def __call__(self, vars_to_derivate, print_expr=True, simplify=False):
+		"""
+		When called, find the derivative, substitutes and prints the expression on latex form
+		"""
+		assert type(vars_to_derivate) is list, "Derivations must be contained in a list: %s is not valid" % vars_to_derivate
+		self._derivate(vars_to_derivate)
+		self.expression_differentiated = self._substitute(self.expression_differentiated)
+		self.expression = self._substitute(self.expression)
+		if simplify: self.expression_differentiated = sy.simplify(self.expression_differentiated)
+		if print_expr:
+			if len(vars_to_derivate) == 1:
+				derivative = 'd'
+			else:
+				derivative = 'd^%d' % (len(vars_to_derivate))
+			print r'\frac{%s}{%s %s} \left(' % (derivative, derivative, sum(i for i in vars_to_derivate)), sy.latex(self.expression), r'\right)'
+			print r'=\\', sy.latex(self.expression_differentiated), r'\\'
+		return self.expression_differentiated
+
 # Symbols
 C 				= sy.Symbol('C')
 a 				= sy.Symbol('a')
@@ -16,16 +61,51 @@ r1_squared 		= sy.Symbol(r'r_1^2')
 r2_squared 		= sy.Symbol(r'r_2^2')
 factor_of_half 	= sy.Symbol(r'\frac{1}{2}')
 
+# Substitution lists
+substitutions_list = [	[sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand],
+						[x1**2 + y1**2, r1_squared],
+						[x2**2 + y2**2, r2_squared]]
+
 # Factors-to-be-differentiated list
 diff_list = [x1,y1,x2,y2]
 
 # Undifferentiated expressions
 r12 			= sy.sqrt((x1-x2)**2 + (y1-y2)**2)
 expression 		= C * sy.exp(-omega*alpha/2.*(x1**2 + y1**2 + x2**2 + y2**2) + a * r12 / (1 + beta * r12))
-subexpression 	= -omega*alpha/2.*(x1**2 + y1**2 + x2**2 + y2**2) + a*r12/(1+beta*r12)
+exponent 		= -omega*alpha/2.*(x1**2 + y1**2 + x2**2 + y2**2) + a*r12/(1+beta*r12)
 last_pt_expr	= a*r12/(1+beta*r12)
 pt_2b_expr		= a*(x1-x2)/(r12*(1+beta*r12))
 denom_r12beta	= 1/(r12*(1+beta*r12))
+
+def derivate_1_over_r12(der):
+	# First derivate of 1/r_12
+	dr12 = DerFinder(1./r12)
+	dr12.add_subs([[sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand]])
+	dr12(der)
+
+def derivate_exponent(der):
+	d_exp = DerFinder(exponent)
+	d_exp.add_subs(substitutions_list)
+	expr = d_exp(der,simplify=True)
+	# print sy.latex(sy.simplify(expr*(-omega*alpha*x1) - omega*alpha))
+
+def derivate_beta_frac(der):
+	d_beta = DerFinder(1/(1+beta*r12))
+	d_beta.add_subs(substitutions_list)
+	d_beta(der,simplify=True)
+
+def derivate_frac(der):
+	d_frac = DerFinder(a*(x1-x2)/(r12*(1+beta*r12)**2))
+	d_frac.add_subs(substitutions_list)
+	d_frac(der,simplify=False)
+
+def double_derivate_exponent(der):
+	d_exp = DerFinder(exponent)
+	# d_exp.add_subs(substitutions_list)
+	d_expr = d_exp(der,simplify=False,print_expr=True)
+	dd_expr = DerFinder(d_expr)
+	dd_expr.add_subs(substitutions_list)
+	dd_expr(der)
 
 def full_derivative():
 	# Differentiated expressions
@@ -41,97 +121,23 @@ def full_derivative():
 	ddpsi_subs3		= ddpsi_subs2.subs(x2**2 + y2**2, r2_squared)
 	return dpsi_subs3
 
-# First derivate of 1/r_12
-def derivate_r12(x):
-	# Derivates one over r12
-	dr12 			= sy.diff(1./r12,x)
-	# Substituting for r12_shorthand
-	dr12 			= dr12.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	return dr12
-
-# First derivative of subexpression
-def derivate_exponent_contents(x):
-	ddx_exp			= sy.diff(subexpression,x)
-	# Substituting back into expression
-	ddx_exp			= ddx_exp.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	ddx_exp 		= ddx_exp.subs(x1**2+y1**2,r1_squared)
-	ddx_exp 		= ddx_exp.subs(x2**2+y2**2,r1_squared)
-	return ddx_exp
-
-# Second derivative of subexpression
-def double_derivate_exponent_contents(x):
-	ddx_exp 		= sy.diff(subexpression,x)
-	ddxddx_exp		= sy.diff(ddx_exp,x)
-	# Substituting back into expression
-	ddxddx_exp		= ddxddx_exp.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	ddxddx_exp 		= ddxddx_exp.subs(x1**2 + y1**2,r1_squared)
-	ddxddx_exp 		= ddxddx_exp.subs(x2**2 + y2**2,r1_squared)
-	return ddxddx_exp
-
-def dx_psi(x):
-	ddx_psi 		= sy.diff(expression,x)
-	# ddx_psi 		= ddx_psi.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	return ddx_psi
-
-def dxdx_psi(x):
-	ddxddx_psi 		= sy.diff(expression,x)
-	ddxddx_psi 		= sy.diff(ddxddx_psi,x)
-	# ddxddx_psi 		= ddxddx_psi.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	return ddxddx_psi
-
-def last_pt_derivate(x):
-	dx_f 			= sy.diff(last_pt_expr, x)
-	# dxdx_f 			= sy.diff(dx_f)
-	dx_f 			= dx_f.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	return dx_f
-
-def derivate_pt_2b(x):
-	dx_pt_2b_expr 	= sy.diff(pt_2b_expr, x)
-	dx_pt_2b_expr	= dx_pt_2b_expr.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	return dx_pt_2b_expr
-
-def derivate_denom_r12beta(x):
-	dx_return 		= sy.diff(denom_r12beta, x)
-	dx_return 		= dx_return.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-	return dx_return
-
 def print_expressions(before,after,x):
 	# Full expression:
 	derivative 		= 'd^2'
 	var 			= x
-	print r'\frac{%s}{%s %s} \big[' % (derivative, derivative, var), sy.latex(before.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)), r'\big]'
+	print r'\frac{%s}{%s %s} \left[' % (derivative, derivative, var), sy.latex(before.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)), r'\big]'
 	print r'=\\', sy.latex(after), r'\\'
 	# print '\n\n\nRaw final: '
 	# print after
 
-var_to_derivate 	= x1
+vars_to_derivate = [x2]
 
-# final_expression 	= full_derivative()
-# final_expression 	= derivate_r12(var_to_derivate)
-# final_expression 	= derivate_exponent_contents(var_to_derivate)
-# final_expression 	= double_derivate_exponent_contents(var_to_derivate)
+def main():
+	# derivate_1_over_r12()
+	# derivate_exponent(vars_to_derivate)
+	# derivate_beta_frac(vars_to_derivate)
+	derivate_frac(vars_to_derivate)
+	# double_derivate_exponent(vars_to_derivate)
 
-final_expression 	= derivate_pt_2b(var_to_derivate)
-print_expressions(pt_2b_expr, final_expression, var_to_derivate)
-
-final_expression 	= derivate_denom_r12beta(var_to_derivate)
-print_expressions(denom_r12beta, final_expression, var_to_derivate)
-
-expr1 = r12*(1+beta*r12)
-print_expressions(expr1, sy.diff(expr1,x1).subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand), x1)
-
-expr2 = a*beta*(x1-x2)/(1+beta*r12)
-print_expressions(expr2, sy.diff(expr2,x1).subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand), x1)
-
-# final_expression	= dxdx_psi(var_to_derivate)
-# final_expression	= final_expression.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand)
-# final_expression	= final_expression.subs((x1 - x2)**2 + (y1 - y2)**2, r12_shorthand**2)
-# final_expression	= final_expression.subs(x1**2 + y1**2,r1_squared)
-# final_expression	= final_expression.subs(x2**2 + y2**2,r2_squared)
-# final_expression	= final_expression/expression
-# print_expressions(subexpression.subs(sy.sqrt((x1-x2)**2 + (y1-y2)**2), r12_shorthand), final_expression, var_to_derivate)
-
-# final_expression 	= last_pt_derivate(var_to_derivate)
-# print_expressions(last_pt_expr, final_expression,var_to_derivate)
-
-# print_expressions(expression, final_expression, var_to_derivate)
+if __name__ == '__main__':
+	main()
